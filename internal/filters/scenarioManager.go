74331b56283b5716f6e3939510b2fb76f30e4a23
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-08-04 12:38:33 +0530

    fix missing data in span metadata

--- internal/filters/scenarioManager.go
+++ internal/filters/scenarioManager.go
@@ -254,7 +254,7 @@ func (scenarioManager *ScenarioManager) getAllRawSpans(tracesForProtocol map[typ
 		for index := range spanForBatch {
 			span := &spanForBatch[index]
 			span.Protocol = string(protocol)
-			if span.Protocol == PHTTP && span.RequestPayload != nil {
+			if (span.Protocol == PHTTP || span.Protocol == PException) && span.RequestPayload != nil {
 				httpRequestPayload := span.RequestPayload.(tracePersistenceModel.HTTPRequestPayload)
 				span.Metadata["request_path"] = httpRequestPayload.ReqPath
 				span.Metadata["method"] = httpRequestPayload.ReqMethod
