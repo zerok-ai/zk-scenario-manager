8e49084877f14ff5c6a25fe54e27007062efbb38
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-08-18 16:11:10 +0530

    update ddl for span and span rawdata

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -19,35 +19,47 @@ CREATE TABLE IF NOT EXISTS incident
     protocol                 VARCHAR(255),
     root_span_time           TIMESTAMP,
     latency_ns               FLOAT,
-    CONSTRAINT unique_issue  UNIQUE (issue_hash, trace_id)
+    CONSTRAINT unique_issue UNIQUE (issue_hash, trace_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS span
 (
-    id               SERIAL PRIMARY KEY,
-    trace_id         VARCHAR(255),
-    span_id          VARCHAR(255),
-    parent_span_id   VARCHAR(255),
-    source           VARCHAR(255),
-    destination      VARCHAR(255),
-    workload_id_list TEXT[],
-    status           VARCHAR(255),
-    metadata         TEXT,
-    latency_ns       FLOAT,
-    protocol         VARCHAR(255),
-    issue_hash_list  TEXT[],
-    time             TIMESTAMP,
-    CONSTRAINT unique_span UNIQUE (trace_id, span_id)
+    id                    SERIAL PRIMARY KEY,
+    trace_id              VARCHAR(255),
+    parent_span_id        VARCHAR(255),
+    span_id               VARCHAR(255),
+    is_root               BOOLEAN,
+    kind                  VARCHAR(255),
+    start_time            TIMESTAMP,
+    latency               DOUBLE PRECISION,
+    source                VARCHAR(255),
+    destination           VARCHAR(255),
+    workload_id_list      TEXT[],
+    protocol              VARCHAR(255),
+    issue_hash_list       TEXT[],
+    request_payload_size  BIGINT,
+    response_payload_size BIGINT,
+    method                VARCHAR(255),
+    route                 VARCHAR(255),
+    scheme                VARCHAR(255),
+    path                  VARCHAR(255),
+    query                 VARCHAR(255),
+    status                INTEGER,
+    metadata              TEXT,
+    username              VARCHAR(255),
+    CONSTRAINT unique_trace_span_id UNIQUE (trace_id, span_id)
 );
 
-
-CREATE TABLE IF NOT EXISTS span_raw_data
+CREATE TABLE span_raw_data
 (
-    id               SERIAL PRIMARY KEY,
-    trace_id         VARCHAR(255),
-    span_id          VARCHAR(255),
-    request_payload  BYTEA,
-    response_payload BYTEA,
-    CONSTRAINT unique_span_raw_data UNIQUE (trace_id, span_id)
+    id           SERIAL PRIMARY KEY,
+    trace_id     VARCHAR(255),
+    span_id      VARCHAR(255),
+    req_headers  TEXT,
+    resp_headers TEXT,
+    is_truncated BOOLEAN,
+    req_body     BYTEA,
+    resp_body    BYTEA,
+    CONSTRAINT unique_span_trace UNIQUE (span_id, trace_id)
 );
