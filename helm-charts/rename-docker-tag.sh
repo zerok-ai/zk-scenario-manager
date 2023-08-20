33cb95ffe2e8f59397dd22f2087092ffb0f6f8b1
avingoyal <avingoyal01+github@gmail.com>
2023-08-20 13:29:09 +0530

    using perl instead of sed

--- helm-charts/rename-docker-tag.sh
+++ helm-charts/rename-docker-tag.sh
@@ -6,5 +6,5 @@ echo "THIS_DIR=$THIS_DIR"
 echo "CHART_VERSION=$CHART_VERSION"
 
 # Set the image tag based on the chart version
-sed -i '' -e "s/tag: \"latest\"/tag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
-sed -i '' -e "s/migrationTag: \"latest\"/migrationTag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
+perl -pi -e "s/tag: \"latest\"/tag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
+perl -pi -e "s/migrationTag: \"latest\"/migrationTag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
