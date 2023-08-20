d7d02ae15a9ef8b318621c7ba8845986507bcfde
avingoyal <avingoyal01+github@gmail.com>
2023-08-20 13:25:39 +0530

    testing

--- helm-charts/rename-docker-tag.sh
+++ helm-charts/rename-docker-tag.sh
@@ -2,6 +2,9 @@
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CHART_VERSION=$1
 
+echo "THIS_DIR=$THIS_DIR"
+echo "CHART_VERSION=$CHART_VERSION"
+
 # Set the image tag based on the chart version
 sed -i '' -e "s/tag: \"latest\"/tag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
 sed -i '' -e "s/migrationTag: \"latest\"/migrationTag: \"$CHART_VERSION\"/" $THIS_DIR/values.yaml
