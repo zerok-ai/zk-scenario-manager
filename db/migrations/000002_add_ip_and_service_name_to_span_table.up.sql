ed21dad0680015e4951a55d626720da643911a87
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-09-14 18:37:02 +0530

    update column size

--- db/migrations/000002_add_ip_and_service_name_to_span_table.up.sql
+++ db/migrations/000002_add_ip_and_service_name_to_span_table.up.sql
@@ -1,8 +1,8 @@
 ALTER TABLE span
-    ADD COLUMN source_ip VARCHAR(30);
+    ADD COLUMN source_ip VARCHAR(255);
 
 ALTER TABLE span
-    ADD COLUMN destination_ip VARCHAR(30);
+    ADD COLUMN destination_ip VARCHAR(255);
 
 ALTER TABLE span
     ADD COLUMN service_name VARCHAR(255);
