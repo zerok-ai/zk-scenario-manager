07e201c1701bb1a2bc0665a60cb4736424930797
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2024-01-05 11:50:25 +0530

    remove postgres and pixie configs

--- config/config.go
+++ config/config.go
@@ -33,12 +33,7 @@ type RouterConfigs struct {
 }
 
 type ScenariosConfig struct {
-	ProcessingIntervalInSeconds int    `yaml:"processingIntervalInSeconds" env-description:"Time interval in seconds for processing scenarios"`
-	EBPFLocalWorkers            int    `yaml:"ebpfLocalWorkers" env-description:"Number of local consumers of eBPF queue"`
-	EBPFSchemaVersion           string `yaml:"ebpfSchemaVersion"`
-	VZCloudAddr                 string `yaml:"vzCloudAddr" env-description:"vz cloud address"`
-	VZClusterId                 string `yaml:"vzClusterId" env-description:"vz cluster id"`
-	VZClusterKey                string `yaml:"vzClusterKey" env-description:"vz cluster key"`
+	ProcessingIntervalInSeconds int `yaml:"processingIntervalInSeconds" env-description:"Time interval in seconds for processing scenarios"`
 }
 
 type WorkLoadConfig struct {
