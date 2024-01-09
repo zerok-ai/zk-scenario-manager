b9fe46bc9f8c2040cd21fc284d576e17dcbcc395
CHNG7 <nikhilchinta7@gmail.com>
2024-01-09 21:39:29 +0530

    minor fix

--- internal/client/ZkOtlpReceiverClient.go
+++ internal/client/ZkOtlpReceiverClient.go
@@ -24,7 +24,7 @@ func GetSpanData(nodeIp string, traceIdPrefixList []string, nodePort string) (ma
 
 	response, zkErr := zkhttp.Create().Post(url, bytes.NewBuffer(requestBody))
 	zklogger.Info(ZkOtlpReceiverLogTag, fmt.Sprintf("Received Status  from OTLP receiver: %s", response.Status))
-	zklogger.Info(ZkOtlpReceiverLogTag, fmt.Sprintf("Received Status code from OTLP receiver: %s", response.StatusCode))
+	zklogger.Info(ZkOtlpReceiverLogTag, fmt.Sprintf("Received Status code from OTLP receiver: %v", response.StatusCode))
 	if zkErr != nil {
 		zklogger.Error(ZkOtlpReceiverLogTag, "Error making HTTP request to OTLP receiver: ", zkErr)
 		return nil, nil //TODO: return error
