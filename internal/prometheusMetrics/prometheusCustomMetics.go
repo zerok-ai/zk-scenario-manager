8f6b0c02a3b728a5716d61c76ad4c2181d3c3974
CHNG7 <nikhilchinta7@gmail.com>
2024-01-11 19:21:20 +0530

    fixed issue with prom labels

--- internal/prometheusMetrics/prometheusCustomMetics.go
+++ internal/prometheusMetrics/prometheusCustomMetics.go
@@ -13,7 +13,7 @@ var (
 			Name: "zerok_sm_root_spans_not_found_total",
 			Help: "No Root Span Found Error",
 		},
-		[]string{"scenario_id", "trace_id"}, // Multiple labels
+		[]string{"scenario", "trace"}, // Multiple labels
 	)
 
 	//rate limited total incidents for each scenario
@@ -22,7 +22,7 @@ var (
 			Name: "zerok_sm_scenario_traces_rate_limited_total",
 			Help: "Total rate limited incidents for each scenario",
 		},
-		[]string{"scenario_id"},
+		[]string{"scenario"},
 	)
 
 	//span count mismatch for incident
@@ -31,7 +31,7 @@ var (
 			Name: "zerok_sm_traces_span_count_mismatch_total",
 			Help: "Total span count mismatch for traces",
 		},
-		[]string{"scenario_id, trace_id"},
+		[]string{"scenario, trace"},
 	)
 
 	//total traces received for scenario to process metric
@@ -40,7 +40,7 @@ var (
 			Name: "zerok_sm_traces_received_for_scenario_to_process_total",
 			Help: "Total traces received for scenario to process",
 		},
-		[]string{"scenario_id"},
+		[]string{"scenario"},
 	)
 
 	//total traces processed by sm and pushed to pipeline metric
@@ -49,7 +49,7 @@ var (
 			Name: "zerok_sm_traces_processed_scenario_total",
 			Help: "Total traces processed by scenario manager for scenario",
 		},
-		[]string{"scenario_id"},
+		[]string{"scenario"},
 	)
 
 	// total spans processed metric for each scenario
@@ -58,7 +58,7 @@ var (
 			Name: "zerok_sm_spans_processed_scenario_total",
 			Help: "Total spans processed by scenario manager for scenario",
 		},
-		[]string{"scenario_id"},
+		[]string{"scenario"},
 	)
 
 	//total export calls failed for scenario
@@ -67,7 +67,7 @@ var (
 			Name: "zerok_sm_export_data_failed_scenario_total",
 			Help: "Total export data failed for scenario",
 		},
-		[]string{"scenario_id"},
+		[]string{"scenario"},
 	)
 
 	//total span data fetch calls success for scenario
@@ -75,26 +75,26 @@ var (
 		Name: "zerok_sm_total_span_data_fetch_calls",
 		Help: "Total spans data fetch calls to receiver by scenario manager",
 	},
-		[]string{"node_ip"})
+		[]string{"nodeip"})
 
 	//total span data fetch calls errors for scenario
 	TotalSpanDataFetchErrors = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "zerok_sm_total_span_data_fetch_errors",
 		Help: "Total spans data fetch errors to receiver by scenario manager",
 	},
-		[]string{"node_ip"})
+		[]string{"nodeip"})
 
 	//total span data fetch calls success for scenario
 	TotalSpanDataFetchSuccess = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "zerok_sm_total_span_data_fetch_success",
 		Help: "Total spans data fetch success to receiver by scenario manager",
 	},
-		[]string{"node_ip"})
+		[]string{"nodeip"})
 
 	//total traces span data requested from receiver
 	TotalTracesSpanDataRequestedFromReceiver = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "zerok_sm_total_traces_span_data_requested_from_receiver",
 		Help: "Total traces span data requested from receiver by scenario manager",
 	},
-		[]string{"node_ip"})
+		[]string{"nodeip"})
 )
